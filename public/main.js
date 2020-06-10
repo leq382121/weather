fc8183d9d04415ab088eb7506a8904a6eeceae5b
Liutauras Razma <leq382121@gmail.com>
2020-06-10 16:39:47 +0300

    small changes

--- public/main.js
+++ public/main.js
@@ -1,11 +1,37 @@
-const { app, BrowserWindow } = require("electron");
+const electron = require("electron");
+const app = electron.app;
+const BrowserWindow = electron.BrowserWindow;
+
+const path = require("path");
+const isDev = require("electron-is-dev");
+
+let mainWindow;
 
 function createWindow() {
-  // Create the browser window.
-  const win = new BrowserWindow({ width: 800, height: 600 });
-
-  // and load the index.html of the app.
-  win.loadURL("http://localhost:3000/");
+  mainWindow = new BrowserWindow({ width: 900, height: 680 });
+  mainWindow.loadURL(
+    isDev
+      ? "http://localhost:3000"
+      : `file://${path.join(__dirname, "../build/index.html")}`
+  );
+  if (isDev) {
+    // Open the DevTools.
+    //BrowserWindow.addDevToolsExtension('<location to your react chrome extension>');
+    mainWindow.webContents.openDevTools();
+  }
+  mainWindow.on("closed", () => (mainWindow = null));
 }
 
 app.on("ready", createWindow);
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
+
+app.on("activate", () => {
+  if (mainWindow === null) {
+    createWindow();
+  }
+});
